1f689e898d71c2d4386913114487865ba70f454e
513sam <101461291+513sam@users.noreply.github.com>
2024-03-14 17:18:09 +0900

    Add files via upload

---
+++ March 13th.py
@@ -0,0 +1,45 @@
+"""x = 100
+
+print(x)
+
+x = 200
+
+print(x)
+
+
+
+
+
+
+x = 100
+
+y = 200
+
+sum = x + y
+
+print("합은",sum)
+
+
+
+
+
+
+Radius = int(input())
+
+area = float(3.14 * Radius * Radius)
+
+print("반지름은",Radius,"원의 넓이는",area)
+
+
+"""
+
+
+print(type(12.30))
+
+print(type("hello")) #으ㅏ노ㅕㅑ오져ㅑㅎ려ㅑㅈ돟려ㅑㅛ모쳐ㅑㅗ먀ㅗ
+
+
+
+
+
+
