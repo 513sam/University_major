8214c98c410232c40dcc59b4bd59f3753caec787
513sam <101461291+513sam@users.noreply.github.com>
2024-03-15 19:53:59 +0900

    Add files via upload

---
+++ March 14th C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("My age : %d \n",20);
+    printf("%d is my point\n",100);
+    printf("Good \nmorning \neverybody\n");
+
+
+}
+
+/*
+/execute console command "/execute as @a at "@s run 
+*/
