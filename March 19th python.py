912c34d5eb98a5f09ce24f91fa07de2c43f4b1bc
513sam <101461291+513sam@users.noreply.github.com>
2024-03-21 16:19:44 +0900

    Add files via upload

---
+++ March 19th python.py
@@ -0,0 +1,62 @@
+"""
+x = 1000
+print("초기값 x=",x)
+x += 2
+print("x+=2 후의 x=",x)
+x -= 2
+print("x-=2 후의 x=",x)
+
+
+"""
+"""
+a = 1000   
+r = 0.05
+n = 10
+
+result = a*(1+r)**n
+
+print("원리금 합계=",result)
+
+
+
+
+x=3.14
+y = int(x)
+print(y)
+
+
+
+
+price = 12345
+tax = price * 0.075
+tax = round(tax,2)
+print(tax)
+
+
+
+message = "철수가 '안녕' 이라고 말했습니다."
+print(message)
+message = '철수가 "안녕" 이라고 말했습니다.'
+print(message)
+
+
+
+
+print("말 한마디로\n천냥빝을 갚는다")
+print("말 한마디로\t천냥빝을 갚는다")
+print("말 한마디로\\천냥빝을 갚는다")
+print("말 한마디로\"천냥빝을 갚는다")
+print("말 한마디로\'천냥빝을 갚는다")
+
+
+s = "Hello World"
+print(s[0])
+print(s[-1])
+"""
+
+name = "Harry Parter"
+lower_name = name.lower()
+
+name = "Harry Parter"
+new_name=name.replace("Parter","Porter")
+
---
+++ March 21th pyhton.py
@@ -0,0 +1,46 @@
+"""
+name = input("이름을 입력하시오: ")
+print(name,"씨, 안녕하세요?")
+print("파이썬에 오신 것을 환영합니다.")
+
+
+"""
+"""
+x = float(input("첫 번쨰정수를 입력하시오:"))
+y = int(input("두 번쨰정수를 입력하`시오:"))
+sum = x + y
+print("합은 ",sum)
+
+
+
+
+
+x = float(input("첫 번쨰정수를 입력하시오:"))
+sum = x
+print("합은 ",sum)
+
+
+
+
+print("안녕하세요?")
+name = input("이름이 어떻게 되시나요?")
+print("만나서 반갑습니다."+ name + "씨")
+print("이름의 길이는 다음과 같군요", len(name))
+
+
+
+age = int(input("나이가 어떻게 되시나요?"))
+print("내년이면 "+str(age+1)+"이 되시는군요")
+
+
+
+
+price = int(input("Enter the price :"))
+if price > 20000:
+    shipping_cost = 0
+else:
+    shipping_cost = 3000
+
+
+"""
+
